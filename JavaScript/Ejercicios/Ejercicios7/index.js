3dec6de08b8971e0bb3fa1e281c112b28cc0179f
Rocio Jimenez <rociojimenezmoreno@gmail.com>
2024-01-22 02:31:37 +0100

    Version del profesor subida.

--- JavaScript/Ejercicios/Ejercicios7/index.js
+++ JavaScript/Ejercicios/Ejercicios7/index.js
@@ -26,7 +26,27 @@
 
 //Version 2
 {
+    const devolverCuadrados = (arr = undefined)=>
+    {
+        if(arr === undefined) return console.warn("No ingesaste un arreglo de numeros.");
+        if(!(arr instanceof Array)) return console.error("El valor que ingresastes no es un arreglo.");
+        if(arr.length===0) return console.error("El arreglo esta vacio.");
+        for(let num of arr)
+        {
+            if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un numero.`);
+        }
 
+        const newArr = arr.map(el => el* el);
+
+        return console.info(`Arreglo original: ${arr}, \nArreglo elevado al cuadrado ${newArr}.`);
+    }
+
+    devolverCuadrados();
+    devolverCuadrados(true);
+    devolverCuadrados({});
+    devolverCuadrados([]);
+    devolverCuadrados([1,"3",4,{}]);
+    devolverCuadrados([1,4]);
 }
 
 //22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
@@ -65,7 +85,24 @@
 
 //Version 2
 {
+    const arrayMinMax = (arr =undefined)=>
+    {
+        if(arr === undefined) return console.warn("No ingesaste un arreglo de numeros.");
+        if(!(arr instanceof Array)) return console.error("El valor que ingresastes no es un arreglo.");
+        if(arr.length===0) return console.error("El arreglo esta vacio.");
+        for(let num of arr)
+        {
+            if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un numero.`);
+        }
 
+        return console.info(`Arregloooooo original: ${arr},\n el valor maximo es ${Math.max(...arr)},\n el valor minimo es ${Math.min(...arr)}`);
+    }
+    arrayMinMax();
+    arrayMinMax(true);
+    arrayMinMax({});
+    arrayMinMax([]);
+    arrayMinMax([1,"3",4,{}]);
+    arrayMinMax([1,4,5,99,-60]);
 }
 
 //23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
@@ -106,5 +143,26 @@
 
 //Version 2
 {
+    console.info("Version 2");
+    const separarParesImpares = (arr =undefined)=>
+    {
+        if(arr === undefined) return console.warn("No ingesaste un arreglo de numeros.");
+        if(!(arr instanceof Array)) return console.error("El valor que ingresastes no es un arreglo.");
+        if(arr.length===0) return console.error("El arreglo esta vacio.");
+        for(let num of arr)
+        {
+            if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un numero.`);
+        }
 
+        return console.info({
+            pares: arr.filter(num => num % 2 === 0),
+            impares: arr.filter(num => num % 2 === 1),
+        });
+    }
+    separarParesImpares();
+    separarParesImpares(true);
+    separarParesImpares({});
+    separarParesImpares([]);
+    separarParesImpares([1,"3",4,{}]);
+    separarParesImpares([1,2,3,4,5,6,7,8,9,0]);
 }
