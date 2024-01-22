d3985e9232a30d1cf4fa3f2ca9e6e3beefcab0bf
Rocio Jimenez <rociojimenezmoreno@gmail.com>
2024-01-22 14:42:33 +0100

    Parte uno temporal de los ejercicios 8.

--- JavaScript/Ejercicios/Ejercicios8/index.js
+++ JavaScript/Ejercicios/Ejercicios8/index.js
@@ -1,7 +1,21 @@
 //24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
 //Version1
 {
+    function Ordenacion (arr = undefined)
+    {
+        if(arr === undefined) return console.warn("No ingesaste un arreglo de numeros.");
+        if(!Array.isArray(mArray)) return console.error("El valor que ingresastes no es un arreglo.");
+        if(arr.length===0) return console.error("El arreglo esta vacio.");
+        for(let num of arr)
+        {
+            if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un numero.`);
+        }
 
+        for(let i = 0; i < arr.length; i++)
+        {
+
+        }
+    }
 }
 
 //Version2
